84c78bbb97deef3cb0adb5f6391b72b399aff551
Amil <amil.masic@gmail.com>
2021-02-26 10:31:15 -0700

    implemented dynamic country dropdown for selection

--- src/index.js
+++ src/index.js
@@ -1,25 +1,57 @@
 const parkUrl = "http://localhost:3000/api/v1/national_parks"
+const createParkForm = document.querySelector("#create-park-form")
 
-document.addEventListener('DOMContentLoaded', () => {
-  getParks();
-  const createParkForm = document.querySelector("#create-park-form")
+    // createParkForm.addEventListener("submit", (e) => createFormHandler(e))
+Array.prototype.contains = function(v) {
+  for (let i = 0; i < this.length; i++) {
+    if (this[i] === v) return true;
+  }
+  return false;
+};
 
-  createParkForm.addEventListener("submit", (e) => createFormHandler(e))
-})
+Array.prototype.unique = function() {
+  let arr = [];
+  for (let i = 0; i < this.length; i++) {
+    if (!arr.contains(this[i])) {
+      arr.push(this[i]);
+    }
+  }
+  return arr;
+}
+
 
 function getParks(){
   fetch(parkUrl)
   .then(response => response.json())
   .then(parks => {
+    countryArray = [];
     parks.data.forEach((park) => {
 
       let newPark = new Park(park, park.attributes)
-
+      // debugger;
       document.querySelector('#park-container').innerHTML += newPark.renderPark()
-    });
 
+        countryArray.push(park.attributes.country.name);
+        // let select = document.getElementById("list");
+        // let newOption = document.createElement("option");
+        // newOption.text = park.attributes.country.name;
+        // select.add(newOption);
+
+    });
+    let uniqueCountries = countryArray.unique();
+    uniqueCountries.forEach((country) => {
+      let select = document.getElementById("list");
+      let newOption = document.createElement("option");
+      newOption.text = country;
+      select.add(newOption);
+    })
   })
 }
+
+getParks();
+
+
+
 
 function createFormHandler(e) {
   e.preventDefault()
@@ -30,6 +62,19 @@ function createFormHandler(e) {
   const inputImage = document.querySelector('#input-url').value
   const inputCity = document.querySelector('#input-city').value
   const countryId = parseInt(document.querySelector('#countries').value)
+  // -------------------------------------------------
+  //  **WIP **
+  // const userInputCountryName = document.querySelector("#txtbox").value
+  // const allParks = Park.all
+  // allParks.forEach(park => {
+  //    if (park.country.name === userInputCountryName) {
+  //     console.log("yes");
+  //    } else {
+  //     console.log("negative");
+  //    }
+  // })
+  // --------------------------------------------------
+  // debugger;
   postPark(parkNameInput, inputEstablished, inputDescription, inputImage, inputCity, countryId)
 
 
@@ -50,11 +95,26 @@ function postPark(name, established, description, image_url, nearest_city, count
   .then(park => {
     const parkData = park.data;
     let newPark = new Park(parkData, parkData.attributes)
-
+    let select = document.getElementById("list");
+    debugger;
     document.querySelector('#park-container').innerHTML += newPark.renderPark();
     document.querySelector("#create-park-form").reset();
   })
 }
+
+
+// function runList() {
+//
+//   let select = document.getElementById("list");
+//   let newOption = document.createElement("option");
+//
+//   newOption.text = document.getElementById("txtbox").value;
+//
+//   select.add(newOption);
+// }
+
+
+
 
 // function renderPark(park) {
 //
