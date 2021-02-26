5c9e6a8332096763e542ec95d019af6eb9d6b102
Amil <amil.masic@gmail.com>
2021-02-26 06:39:45 -0700

    added classes to rendered parks

--- src/park.js
+++ src/park.js
@@ -17,13 +17,13 @@ class Park {
     <div data-id=${this.id}>
     <img src=${this.image_url} height="300" width="450">
     <br>
-    <h3> Park Name: ${this.name}</h3>
-    <p>Established in: ${this.established}</p>
-    <p>The Nearest City: ${this.nearest_city}</p>
-    <p country-name>Country: ${this.country.name}</p>
-    <p continent>Contient: ${this.country.continent}</p>
-    <p>Oficial Langauge: ${this.country.language}</p>
-    <p>Short Description: ${this.description}</p>
+    <h3 class="park-name"> Park Name: ${this.name}</h3>
+    <p class="established">Established in: ${this.established}</p>
+    <p class="nearest-city">The Nearest City: ${this.nearest_city}</p>
+    <p class="country-name">Country: ${this.country.name}</p>
+    <p class="continent">Contient: ${this.country.continent}</p>
+    <p class="language">Oficial Langauge: ${this.country.language}</p>
+    <p class="description">Short Description: ${this.description}</p>
     </div>
     <br>`;
   }
