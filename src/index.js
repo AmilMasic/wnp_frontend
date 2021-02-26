e3f06f72aeed32fa6a42e223111160188d2f590a
Amil <amil.masic@gmail.com>
2021-02-26 10:48:32 -0700

    removed whitespaces and old code that was commented out

--- src/index.js
+++ src/index.js
@@ -1,23 +1,7 @@
 const parkUrl = "http://localhost:3000/api/v1/national_parks"
 const createParkForm = document.querySelector("#create-park-form")
 
-    // createParkForm.addEventListener("submit", (e) => createFormHandler(e))
-Array.prototype.contains = function(v) {
-  for (let i = 0; i < this.length; i++) {
-    if (this[i] === v) return true;
-  }
-  return false;
-};
 
-Array.prototype.unique = function() {
-  let arr = [];
-  for (let i = 0; i < this.length; i++) {
-    if (!arr.contains(this[i])) {
-      arr.push(this[i]);
-    }
-  }
-  return arr;
-}
 
 
 function getParks(){
@@ -28,14 +12,9 @@ function getParks(){
     parks.data.forEach((park) => {
 
       let newPark = new Park(park, park.attributes)
-      // debugger;
       document.querySelector('#park-container').innerHTML += newPark.renderPark()
 
         countryArray.push(park.attributes.country.name);
-        // let select = document.getElementById("list");
-        // let newOption = document.createElement("option");
-        // newOption.text = park.attributes.country.name;
-        // select.add(newOption);
 
     });
     let uniqueCountries = countryArray.unique();
@@ -55,7 +34,6 @@ getParks();
 
 function createFormHandler(e) {
   e.preventDefault()
-  // debugger;
   const parkNameInput = document.querySelector('#input-park-name').value
   const inputEstablished =  document.querySelector('#input-established').value
   const inputDescription =  document.querySelector('#input-description').value
@@ -81,8 +59,6 @@ function createFormHandler(e) {
 }
 
 function postPark(name, established, description, image_url, nearest_city, country_id) {
-  // console.log(name, established, description, image_url, nearest_city, country_id);
-
   let bodyData = {name, established, description, image_url, nearest_city, country_id}
 
   fetch(parkUrl, {
@@ -113,24 +89,19 @@ function postPark(name, established, description, image_url, nearest_city, count
 //   select.add(newOption);
 // }
 
+Array.prototype.contains = function(v) {
+  for (let i = 0; i < this.length; i++) {
+    if (this[i] === v) return true;
+  }
+  return false;
+};
 
-
-
-// function renderPark(park) {
-//
-//   const parkMarkup = `
-//   <div data-id=${park.id}>
-//   <img src=${park.attributes.image_url} height="300" width="450">
-//   <br>
-//   <h3> Park Name: ${park.attributes.name}</h3>
-//   <p country-name>Country: ${park.attributes.country.name}</p>
-//   <p>Oficial Langauge: ${park.attributes.country.language}</p>
-//   <p>Established in: ${park.attributes.established}</p>
-//   <p>Short Description: ${[park.attributes.description]}</p>
-//   <p>The Nearest City: ${[park.attributes.nearest_city]}</p>
-//   </div>
-//   <br>
-//   `;
-//
-//   document.querySelector('#park-container').innerHTML += parkMarkup
-// }
+Array.prototype.unique = function() {
+  let arr = [];
+  for (let i = 0; i < this.length; i++) {
+    if (!arr.contains(this[i])) {
+      arr.push(this[i]);
+    }
+  }
+  return arr;
+}
